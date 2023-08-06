c4790843a1484569ea1fff9a1aa84f50406a0085
Abner Freitas <defreitasabner@gmail.com>
2023-08-06 10:38:25 -0300

    Implementado tratamento de erro para MethodArgumentNotValidException retornando 400 (bad request).

--- src/main/java/freitas/abner/expenses/infrastructure/ErrorHandler.java
+++ src/main/java/freitas/abner/expenses/infrastructure/ErrorHandler.java
@@ -2,6 +2,8 @@ package freitas.abner.expenses.infrastructure;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,18 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity error404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException ex) {
+        var fieldErrors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(ValidationErrorData::new).toList());
+    }
+
+    record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 
 }
