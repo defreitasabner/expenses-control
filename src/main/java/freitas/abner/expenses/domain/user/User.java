f481b0601ac2d1d53b38bead8054f344c554d2ce
Abner Freitas <defreitasabner@gmail.com>
2024-01-09 19:01:31 -0300

    Implemented entity User.

---
+++ src/main/java/freitas/abner/expenses/domain/user/User.java
@@ -0,0 +1,42 @@
+package freitas.abner.expenses.domain.user;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+
+@Table(name = "users")
+@Entity(name = "User")
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
